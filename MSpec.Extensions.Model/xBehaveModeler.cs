cb4059d6cdeb4c5d2293f0c3e60ee6105e363f75
dotnetprofessional <github@dotnetprofessional.com>
2014-04-09 21:38:27 -0700

    Removed references to xpf.IO and incorporated the needed code to eliminate the dependencies.

--- MSpec.Extensions.Model/xBehaveModeler.cs
+++ MSpec.Extensions.Model/xBehaveModeler.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-using Machine.Specifications;
 using MSpec.Extensions.Model.MSpec;
 using MSpec.Extensions.Model.xBehave;
 using xpf.IO;
---
+++ MSpec.Extensions.Model/xpf/Convert.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace xpf.IO
+{
+    internal static class Convert
+    {
+        public static string ToXml<T>(this T instance, params Type[] extraTypes)
+        {
+            string xml = "";
+            using (var ms = new MemoryStream())
+            {
+                var xser = new XmlSerializer(typeof(T), extraTypes);
+                xser.Serialize(ms, instance);
+
+                ms.Position = 0;
+                using (var s = new StreamReader(ms))
+                {
+                    xml = s.ReadToEnd();
+                }
+            }
+            return xml;
+        }
+
+        public static T FromXmlToInstance<T>(this string xml, params Type[] extraTypes)
+            where T : class
+        {
+            return new MemoryStream(Encoding.UTF8.GetBytes(xml)).FromXmlStreamToInstance<T>();
+        }
+
+        public static T FromXmlStreamToInstance<T>(this Stream stream, params Type[] extraTypes)
+            where T : class
+        {
+            T entity;
+            using (stream)
+            {
+                var xser = new XmlSerializer(typeof(T), extraTypes);
+                entity = xser.Deserialize(stream) as T;
+            }
+            return entity;
+        }
+    }
+}
